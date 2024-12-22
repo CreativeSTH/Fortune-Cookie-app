09064f37eee607195181db662edfe3f44655bcb2
CreativeSTH <sth.frontend@gmail.com>
2024-12-21 21:40:47 -0500

    Update Hero Component

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,10 +1,28 @@
 import '../styles/Hero.css';
+import cookie from '/img/cookie.webp';
+import monster from '/img/monster.webp';
+import iconScroll from '/img/icon.svg';
 
 const Hero = () => {
     return (
-        <div>
-            <h1>Hero</h1>
-        </div>
+        <> 
+            <section className='container'>
+                <article className='hero__card__glass'>
+                    <img className='hero__card__img' src={monster}  alt="The cookie monster"/>
+                    <h1 className='hero__card__title'>GALLETAS</h1>
+                    <div className='hero__card__title--effect'>GALLETAS</div>
+                    <h2 className='hero__card__subtitle'>DE LA FORTUNA</h2>
+                    <div className='hero__card__subtitle--effect'>DE LA FORTUNA</div>
+                </article>
+                <article className='hero__imgs'>
+                    <img className='imgs__galleta__blur--a' src={cookie} alt="a Lucky Cookie" />
+                    <img className='imgs__galleta__blur--b' src={cookie} alt="a Lucky Cookie" />
+                    <img className='imgs__galleta__blur--c' src={cookie} alt="a Lucky Cookie" />
+                    <img className='imgs__icon__scroll' src={iconScroll} alt="This is an icon that indicates scroll" />
+                    <div className='moon'></div>
+                </article>
+            </section>
+        </>
     );
 };
 
--- src/components/LuckyCookie.jsx
+++ src/components/LuckyCookie.jsx
@@ -2,7 +2,7 @@ import '../styles/LuckyCookie.css';
 
 const LuckyCookie = () => {
   return (
-    <div>
+    <div className='lucky-cookie'>
       <h1>Lucky Cookie</h1>
       <p>Click the cookie to get your fortune!</p>
     </div>
